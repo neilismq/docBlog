f76da195bd2b0f0165a95ee3fbb69c5b2d86b0ef
zhaozhiqiang35 <zhaozhiqiang35@jd.com>
2021-10-02 22:10:30 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+const head = require('./config/head.js');
+const plugins = require('./config/plugins.js');
+const themeConfig = require('./config/themeConfig.js');
+
+module.exports = {
+  theme: 'vdoing', // 使用npm包主题
+  // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
+
+  title: "neilism's blog",
+  description: 'Java技术博客，积跬步以至千里，致敬每个坚持学习的你',
+  // base: '/', // 格式：'/<仓库名>/'， 默认'/'
+  markdown: {
+    lineNumbers: true, // 代码行号
+  },
+
+  head,
+  plugins,
+  themeConfig,
+}
---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,23 @@
+// nav
+module.exports = [
+  { text: '首页', link: '/' },
+  {
+    text: 'Java基础', 
+    link: '/javaBase/',
+    item:[
+      {
+        text:'HashMap原理',
+        item:[
+          {
+            text:'HashMap原理1',
+            link:'/pages/21aaf1/'
+          },
+          {
+            text:'HashMap原理2',
+            link:'/pages/21aaf2/'
+          }
+        ]
+      }
+    ]
+  },
+]

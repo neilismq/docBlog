8a079b914027666214c26bddb8db4d8e16024575
zhaozhiqiang35 <zhaozhiqiang35@jd.com>
2021-10-02 23:13:18 +0800

    fix

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -2,22 +2,33 @@
 module.exports = [
   { text: '首页', link: '/' },
   {
-    text: 'Java基础', 
-    link: '/javaBase/',
-    item:[
+    text: '工作日志',
+    link: '/workLog/'
+  },
+  {
+    text: 'Java基础',
+    link: '/javaBase/'
+  },
+  {
+    text: '常见中间件',
+    link:'/popularMedContains',
+    items: [
       {
-        text:'HashMap原理',
-        item:[
-          {
-            text:'HashMap原理1',
-            link:'/pages/21aaf1/'
-          },
-          {
-            text:'HashMap原理2',
-            link:'/pages/21aaf2/'
-          }
-        ]
+        text: 'Redis',
+        link: '/pages/ee8dbb/',
       }
     ]
+  },
+  {
+    text: '分类',
+    link: '/categories/'
+  },
+  {
+    text: '标签',
+    link: '/tags/'
+  },
+  {
+    text: '归档',
+    link: '/archives/'
   },
 ]
---
+++ docs/_posts/随笔/TODO.md
@@ -0,0 +1,15 @@
+---
+title: TODO
+date: 2021-10-02 22:50:01
+permalink: /pages/3befb8/
+sidebar: auto
+categories:
+  - 随笔
+tags:
+  - 
+---
+### 本博客待做项
+- 评论系统
+- 颜色修改
+- 自动部署github.io
+- 图片替换

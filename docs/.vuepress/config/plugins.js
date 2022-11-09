dc03acbd5b77ea41eb4aeca65e3129707893c80d
zhaozhiqiang35 <zhaozhiqiang35@jd.com>
2022-11-09 15:12:34 +0800

    fix

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -51,20 +51,20 @@ module.exports = [
       showInMobile: false, // whether to display on the mobile side, default: false.
     },
   ],
-  [
-    'demo-block',
-    {
-      // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
-      settings: {
-        // jsLib: ['http://xxx'], // 在线示例(jsfiddle, codepen)中的js依赖
-        // cssLib: ['http://xxx'], // 在线示例中的css依赖
-        // vue: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js', // 在线示例中的vue依赖
-        jsfiddle: false, // 是否显示 jsfiddle 链接
-        codepen: true, // 是否显示 codepen 链接
-        horizontal: false, // 是否展示为横向样式
-      },
-    },
-  ],
+  // [
+  //   'demo-block',
+  //   {
+  //     // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
+  //     settings: {
+  //       // jsLib: ['http://xxx'], // 在线示例(jsfiddle, codepen)中的js依赖
+  //       // cssLib: ['http://xxx'], // 在线示例中的css依赖
+  //       // vue: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js', // 在线示例中的vue依赖
+  //       jsfiddle: false, // 是否显示 jsfiddle 链接
+  //       codepen: true, // 是否显示 codepen 链接
+  //       horizontal: false, // 是否展示为横向样式
+  //     },
+  //   },
+  // ],
   [
     'vuepress-plugin-zooming', // 放大图片
     {
@@ -74,12 +74,12 @@ module.exports = [
       },
     },
   ],
-  [
-    'vuepress-plugin-baidu-tongji', // 百度统计
-    {
-      hm: '503f098e7e5b3a5b5d8c5fc2938af002',
-    },
-  ],
+  // [
+  //   'vuepress-plugin-baidu-tongji', // 百度统计
+  //   {
+  //     hm: 'a671ceacce603249c64801c7f9ca627a',
+  //   },
+  // ],
   [
     'vuepress-plugin-comment', // 评论
     // {
